fbc0c94f973406d7d6522f86b05ca0a2cc81a1c3
ShangLin <38915581+Viper603@users.noreply.github.com>
2018-05-02 13:00:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ShangLinWindow window = new ShangLinWindow(900, 800);
+        window.setVisible(true);
+
+    }
+
+}
---
+++ src/ShangLinWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.Frame;
+public class ShangLinWindow extends JFrame{
+    String name;
+    public ShangLinWindow(int w,int h){
+
+        setSize(w,h);
+
+    }
+}
